03c68a08be9a3dcad57f5fe85edaed3e0cd10b91
Christopher Tran <christopher.tran@10xgenomics.com>
2023-02-01 21:51:53 -0800

    test sentry

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,34 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://2253eea21c524342863e02c1af230c41@o299784.ingest.sentry.io/6293879",
+
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+  // If the entire session is not sampled, use the below sample rate to sample
+  // sessions when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+
+  integrations: [new Sentry.Replay()],
+});
 
 export default function Home() {
+  const asdf = () => {
+    throw "testing sentry";
+  };
+
   return (
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <div onClick={() => asdf()}>BREAK IT</div>
 
       <main>
         <Header title="Welcome to my app!" />
@@ -19,5 +39,5 @@ export default function Home() {
 
       <Footer />
     </div>
-  )
+  );
 }
